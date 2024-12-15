56a4ba6f641d1b4df46156d624c85a0cdcf205e9
sinanisler <sinan@sinanisler.com>
2024-12-15 23:31:04 +0300

    color sync idea

--- includes/color-sync-bricks-and-block-editor.php
+++ includes/color-sync-bricks-and-block-editor.php
@@ -1,4 +1,4 @@
 <?php 
-// Idea is to Sync Primery Color and Primary Font from the default bricks theme typography setting.
+// Idea is to Sync Primary Color, BG Color and Primary Font from the default bricks theme typography setting.
 // https://academy.bricksbuilder.io/article/filter-color-palette/
 // https://academy.bricksbuilder.io/?s=color
