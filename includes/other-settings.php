77b5011a4615d71af090f83e4ed84993fd80b3a0
sinanisler <sinan@sinanisler.com>
2024-12-28 02:21:29 +0300

    disable comment setting and some mini fixes

--- includes/other-settings.php
+++ includes/other-settings.php
@@ -109,6 +109,16 @@ function snn_enqueue_gsap_callback() {
     $options = get_option('snn_other_settings');
     ?>
     <input type="checkbox" name="snn_other_settings[enqueue_gsap]" value="1" <?php checked(1, isset($options['enqueue_gsap']) ? $options['enqueue_gsap'] : 0); ?>>
+    <p>
+        Enabling this setting will enqueue the GSAP library and its associated scripts on your website.<br>
+        GSAP is a powerful JavaScript animation library that allows you to create complex and interactive animations.<br><br>
+        - Ability to create GSAP animations with just data-animate attributes.<br>
+        - gsap.min.js: The core GSAP library.<br>
+        - ScrollTrigger.min.js: A GSAP plugin that enables scroll-based animations.<br>
+        - gsap-data-animate.js: A custom script that utilizes GSAP and ScrollTrigger for animating elements based on data attributes.<br><br>
+        Read <a href="https://github.com/sinanisler/snn-brx-child-theme/wiki/GSAP-ScrollTrigger-Animations" target="_blank">
+            Documentation and Examples</a> for more details.
+    </p>
     <?php
 }
 
@@ -116,7 +126,7 @@ function snn_revisions_limit_callback() {
     $options = get_option('snn_other_settings');
     $value = isset($options['revisions_limit']) ? intval($options['revisions_limit']) : '';
     ?>
-    <input type="number" name="snn_other_settings[revisions_limit]" value="<?php echo esc_attr($value); ?>" placeholder="500">
+    <input type="number" name="snn_other_settings[revisions_limit]" value="<?php echo esc_attr($value); ?>" placeholder="500" min="0">
     <?php
 }
 
@@ -138,18 +148,71 @@ function snn_disable_comments_callback() {
     $options = get_option('snn_other_settings');
     ?>
     <label>
-    <input type="checkbox" name="snn_other_settings[disable_comments]" value="1" <?php checked(1, isset($options['disable_comments']) ? $options['disable_comments'] : 0); ?> >
+        <input type="checkbox" name="snn_other_settings[disable_comments]" value="1" <?php checked(1, isset($options['disable_comments']) ? $options['disable_comments'] : 0); ?> >
+        Disable all comments on the site
     </label>
     <?php
 }
+
+function snn_enqueue_gsap_scripts() {
+    $options = get_option('snn_other_settings');
+    if (isset($options['enqueue_gsap']) && $options['enqueue_gsap']) {
+        wp_enqueue_script('gsap-js', get_stylesheet_directory_uri() . '/js/gsap.min.js', array(), null, true);
+        wp_enqueue_script('gsap-st-js', get_stylesheet_directory_uri() . '/js/ScrollTrigger.min.js', array('gsap-js'), null, true);
+        wp_enqueue_script('gsap-data-js', get_stylesheet_directory_uri() . '/js/gsap-data-animate.js?v0.01', array(), null, true);
+    }
+}
+add_action('wp_enqueue_scripts', 'snn_enqueue_gsap_scripts');
+
+function snn_limit_post_revisions($num, $post) {
+    $options = get_option('snn_other_settings');
+    if (isset($options['revisions_limit']) && intval($options['revisions_limit']) > 0) {
+        return intval($options['revisions_limit']);
+    }
+    return $num;
+}
+add_filter('wp_revisions_to_keep', 'snn_limit_post_revisions', 10, 2);
+
+function snn_auto_update_bricks_theme($update, $item) {
+    $options = get_option('snn_other_settings');
+    if (isset($options['auto_update_bricks']) && $options['auto_update_bricks'] && isset($item->theme) && $item->theme === 'bricks') {
+        return true;
+    }
+    return $update;
+}
+add_filter('auto_update_theme', 'snn_auto_update_bricks_theme', 10, 2);
+
+function snn_custom_menu_order($menu_ord) {
+    $options = get_option('snn_other_settings');
+    if (isset($options['move_bricks_menu']) && $options['move_bricks_menu']) {
+        if (!$menu_ord) return true;
+        $bricks_menu = null;
+        foreach ($menu_ord as $index => $item) {
+            if ($item === 'bricks') {
+                $bricks_menu = $item;
+                unset($menu_ord[$index]);
+                break;
+            }
+        }
+        if ($bricks_menu) {
+            $menu_ord[] = $bricks_menu;
+        }
+        return $menu_ord;
+    }
+    return $menu_ord;
+}
+add_filter('menu_order', 'snn_custom_menu_order');
+add_filter('custom_menu_order', '__return_true');
 
 function snn_hide_comments_section() {
     $options = get_option('snn_other_settings');
     if (isset($options['disable_comments']) && $options['disable_comments']) {
         echo '<style>#menu-comments { display: none !important; }</style>';
-        update_option('comment_registration', 1); 
+        update_option('comment_registration', 1);
+        add_filter('comments_open', '__return_false', 20, 2);
+        add_filter('pings_open', '__return_false', 20, 2);
     } else {
-        update_option('comment_registration', 0); 
+        update_option('comment_registration', 0);
     }
 }
 add_action('admin_head', 'snn_hide_comments_section');
