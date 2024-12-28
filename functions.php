df47d3b61caf3cf8e9581146e92cf88f76efc929
sinanisler <sinan@sinanisler.com>
2024-12-28 23:37:18 +0300

    functions.php cleanup and organizing

--- functions.php
+++ functions.php
@@ -1,41 +1,7 @@
 <?php
- 
-// Frontend Head Inline JS and CSS, This will load in the <head>
-// ASCII ART https://patorjk.com/software/taag/#p=display&f=ANSI%20Regular&t=sinanisler.com
-function snn_custom_inline_frontend_head_code() {    ?>
-<style type="text/css">
 
-</style>
-<script>
-
-</script>
-<?php }
-add_action( 'wp_head', 'snn_custom_inline_frontend_head_code', 1 );
-
-
-// Frontend Footer Inline JS and CSS,  This will load just before the </body>
-function snn_custom_footer_inline() { ?>
-<style>
-
-</style>
-<script>
-
-</script>
-<?php }
-add_action('wp_footer', 'snn_custom_footer_inline', 9999);
-
-
-// WP-Admin Backend Custom JS and CSS in <head>
-function sinn_custom_css() { ?>
-<style>
-
-</style>
-<script>
-
-</script>
-<?php }
-add_action('admin_head', 'sinn_custom_css');
-
+// Use CUSTOM-CODES-HERE.php file for adding your custom codes. 
+require_once get_stylesheet_directory() . '/CUSTOM-CODES-HERE.php';
 
 
 
