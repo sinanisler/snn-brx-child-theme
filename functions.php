e46223122df3d018bcab1698664c934b181d6560
sinanisler <sinan@sinanisler.com>
2024-12-29 13:28:58 +0300

    optimization for element registers

--- functions.php
+++ functions.php
@@ -35,40 +35,81 @@ require_once get_stylesheet_directory() . '/dynamic_data_tags/custom_dynamic_dat
 
 
 
-add_action( 'init', function() {
-    $elements = [
-        [
-            'file' => get_stylesheet_directory() . '/custom_elements/custom-html-css-script.php',
-            'slug' => 'custom-html-css-script',
-            'class' => 'Custom_HTML_CSS_Script',
-        ],
 
 
-        [
-            'file' => get_stylesheet_directory() . '/custom_elements/custom-maps.php',
-            'slug' => null,
-            'class' => null,
-        ],
+add_action('init', function () {
+    $custom_html_css_script_file = get_stylesheet_directory() . '/custom_elements/custom-html-css-script.php';
+    if (file_exists($custom_html_css_script_file)) {
+        require_once $custom_html_css_script_file;
+        \Bricks\Elements::register_element($custom_html_css_script_file, 'custom-html-css-script', 'Custom_HTML_CSS_Script');
+    }
 
-        /*
-        [
-            'file' => get_stylesheet_directory() . '/custom_elements/custom-button.php',
-            'slug' => null,
-            'class' => null,
-        ],
-        */
+    $custom_maps_file = get_stylesheet_directory() . '/custom_elements/custom-maps.php';
+    if (file_exists($custom_maps_file)) {
+        require_once $custom_maps_file;
+        \Bricks\Elements::register_element($custom_maps_file);
+    }
 
-
-    ];
-
-    foreach ( $elements as $element ) {
-        if ( file_exists( $element['file'] ) ) {
-            require_once $element['file'];
-            if ( $element['slug'] && $element['class'] ) {
-                \Bricks\Elements::register_element( $element['file'], $element['slug'], $element['class'] );
-            } else {
-                \Bricks\Elements::register_element( $element['file'] );
-            }
+    $options = get_option('snn_other_settings');
+    if (isset($options['enqueue_gsap']) && $options['enqueue_gsap']) {
+        $lottie_animation_file = get_stylesheet_directory() . '/custom_elements/lottie-animation.php';
+        if (file_exists($lottie_animation_file)) {
+            require_once $lottie_animation_file;
+            \Bricks\Elements::register_element($lottie_animation_file);
         }
     }
-}, 11 );
+
+
+
+}, 11);
+
+
+
+
+
+
+// Enable JSON file uploads in the Media Library
+function allow_json_upload($mimes) {
+    // Add .json to the list of allowed mime types
+    $mimes['json'] = 'application/json';
+    return $mimes;
+}
+add_filter('upload_mimes', 'allow_json_upload');
+
+// Fix MIME type check for JSON uploads (for security and compatibility)
+function check_json_filetype($data, $file, $filename, $mimes) {
+    // Get the file extension
+    $filetype = wp_check_filetype($filename, $mimes);
+    
+    // If the extension is JSON, update the type and ext
+    if ($filetype['ext'] === 'json') {
+        $data['ext'] = 'json';
+        $data['type'] = 'application/json';
+    }
+
+    return $data;
+}
+add_filter('wp_check_filetype_and_ext', 'check_json_filetype', 10, 4);
+
+// Allow JSON files to bypass the upload restriction in WordPress
+function allow_unfiltered_json_upload($file) {
+    // Check for JSON file type
+    if ($file['type'] === 'application/json') {
+        // No error for JSON files
+        $file['error'] = false;
+    }
+    return $file;
+}
+add_filter('wp_handle_upload_prefilter', 'allow_unfiltered_json_upload');
+
+// Display JSON files properly in the Media Library (optional)
+function enable_json_preview_in_media_library($response, $attachment, $meta) {
+    // Ensure the file is a JSON file
+    if ($response['mime'] === 'application/json') {
+        // Provide a basic preview message for JSON files
+        $response['uploaded_filename'] = basename(get_attached_file($attachment->ID));
+        $response['url'] = wp_get_attachment_url($attachment->ID);
+    }
+    return $response;
+}
+add_filter('wp_prepare_attachment_for_js', 'enable_json_preview_in_media_library', 10, 3);
