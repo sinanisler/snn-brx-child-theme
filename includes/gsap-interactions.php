38f4ff5e31580645d9ee13fc196ebd887086e15f
sinanisler <sinan@sinanisler.com>
2024-12-29 23:59:30 +0300

    Laying the foundations for future

---
+++ includes/gsap-interactions.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+
+    Laying the foundations for future
+
+
+    https://academy.bricksbuilder.io/article/interactions/
+
+    
+    
+    This will be implemented after I am sure 100% gsap-data-animate.js is stable
+
+
+
+
+*/
+
+
+
