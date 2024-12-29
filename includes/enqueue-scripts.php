0228192e69cd3ea697f6fabfea20507823fc760d
sinanisler <sinan@sinanisler.com>
2024-12-29 19:34:35 +0300

    working on gsap ux for complex animations

--- includes/enqueue-scripts.php
+++ includes/enqueue-scripts.php
@@ -5,3 +5,23 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('bricks-child', get_stylesheet_uri(), ['bricks-frontend'], filemtime(get_stylesheet_directory() . '/style.css'));
   }
 });
+
+
+
+// Check if the user is logged in and the URL '?bricks=run'
+function add_footer_inline_js_for_logged_users() {
+  if (is_user_logged_in() && isset($_GET['bricks']) && $_GET['bricks'] === 'run') {
+      add_action('wp_footer', function() {
+?>
+<script>
+
+
+
+
+
+</script>
+<?php
+      });
+  }
+}
+add_action('wp', 'add_footer_inline_js_for_logged_users');
