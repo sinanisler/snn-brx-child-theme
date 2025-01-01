0bee610307945464cd29b830d0e6be96a409398f
sinanisler <sinan@sinanisler.com>
2025-01-01 23:46:42 +0300

    wording change

--- custom_elements/custom-html-css-script.php
+++ custom_elements/custom-html-css-script.php
@@ -1,29 +1,23 @@
 <?php 
 if ( ! defined( 'ABSPATH' ) ) exit;
-
-
-
 class Custom_HTML_CSS_Script extends \Bricks\Element {
   public $category     = 'snn';
   public $name         = 'custom-html-css-script';
   public $icon         = 'fas fa-code'; 
   public $css_selector = '.snn-brx-html-css-script-wrapper';
-
   public function get_label() {
     return 'Custom Code HTML -SNN';
   }
 
-  // Set builder control
   public function set_controls() {
     $this->controls['content'] = [
       'tab' => 'content',
-      'label' => 'Custom HTML Code',
+      'label' => 'Custom HTML CSS JS Code',
       'type' => 'code',
       'mode' => 'php',
-      'default' => '<div>Your HTML here</div>
-
+      'default' => '
+<div>Your HTML here</div>
 <h1 class="redme">Title</h1>
-
 <style>
 .redme{color:red}
 </style>
@@ -31,12 +25,10 @@ class Custom_HTML_CSS_Script extends \Bricks\Element {
     ];
   }
 
-  // Enqueue element styles and scripts
   public function enqueue_scripts() {
     // Optionally enqueue styles or scripts
   }
 
-  // Render element HTML
   public function render() {
     echo "<div class='custom-html-css-script-wrapper' style='width:100%'>";
     echo $this->settings['content']; // Direct output, consider security implications
@@ -44,7 +36,6 @@ class Custom_HTML_CSS_Script extends \Bricks\Element {
   }
 }
 
-// Register the custom element
 add_action( 'bricks/element_classes', function( $element_classes ) {
   $element_classes['custom-html-css-script'] = 'Custom_HTML_CSS_Script';
   return $element_classes;
