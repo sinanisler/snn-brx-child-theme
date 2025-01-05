9215ae34041e665a21ffdfa8cc667a8210f7e862
sinanisler <sinan@sinanisler.com>
2025-01-05 20:35:56 +0300

    IMPROVEMENT: updated code element description and default codes to make it clear it can accept html, css and js.

--- custom_elements/custom-html-css-script.php
+++ custom_elements/custom-html-css-script.php
@@ -12,15 +12,25 @@ class Custom_HTML_CSS_Script extends \Bricks\Element {
   public function set_controls() {
     $this->controls['content'] = [
       'tab' => 'content',
-      'label' => 'Custom HTML CSS JS Code',
+      'label' => '
+Custom HTML CSS JS Code<br>Just write your HTML or CSS or JS here.<br> 
+To use JS use < <a href="https://www.w3schools.com/tags/tag_script.asp" target="_blank" title="Learn Script Tag">script</a> > tags and 
+for CSS use < <a href="https://www.w3schools.com/tags/tag_style.asp" target="_blank" title="Learn Style Tag">style</a> > tags.
+      ',
       'type' => 'code',
       'mode' => 'php',
       'default' => '
 <div>Your HTML here</div>
+
 <h1 class="redme">Title</h1>
+
 <style>
 .redme{color:red}
 </style>
+
+<script>
+var sebas_sama = 6969;
+</script>
       ',
     ];
   }
