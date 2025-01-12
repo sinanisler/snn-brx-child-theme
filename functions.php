64949d23ac677c83afc5ba7aa6fabae917c9a02f
sinanisler <sinan@sinanisler.com>
2025-01-12 16:41:18 +0300

    restructing utils

--- functions.php
+++ functions.php
@@ -20,7 +20,6 @@ require_once get_stylesheet_directory() . '/includes/disable-file-editing.php';
 require_once get_stylesheet_directory() . '/includes/remove-rss.php';
 require_once get_stylesheet_directory() . '/includes/disable-wp-json-if-not-logged-in.php';
 require_once get_stylesheet_directory() . '/includes/login-logo-change-url-change.php';
-// require_once get_stylesheet_directory() . '/includes/theme-json-styles.php';
 require_once get_stylesheet_directory() . '/includes/enqueue-scripts.php';
 require_once get_stylesheet_directory() . '/includes/file-size-column-media.php';
 require_once get_stylesheet_directory() . '/includes/404-logging.php';
@@ -41,6 +40,12 @@ require_once get_stylesheet_directory() . '/dynamic_data_tags/custom-dynamic-dat
 
 
 
+
+
+
+
+
+// Register Custom Bricks Builder Elements
 add_action('init', function () {
     $custom_html_css_script_file = get_stylesheet_directory() . '/custom_elements/custom-html-css-script.php';
     if (file_exists($custom_html_css_script_file)) {
@@ -74,51 +79,4 @@ add_action('init', function () {
 
 
 
-
-
-
-// Enable JSON file uploads in the Media Library
-function allow_json_upload($mimes) {
-    // Add .json to the list of allowed mime types
-    $mimes['json'] = 'application/json';
-    return $mimes;
-}
-add_filter('upload_mimes', 'allow_json_upload');
-
-// Fix MIME type check for JSON uploads (for security and compatibility)
-function check_json_filetype($data, $file, $filename, $mimes) {
-    // Get the file extension
-    $filetype = wp_check_filetype($filename, $mimes);
-    
-    // If the extension is JSON, update the type and ext
-    if ($filetype['ext'] === 'json') {
-        $data['ext'] = 'json';
-        $data['type'] = 'application/json';
-    }
-
-    return $data;
-}
-add_filter('wp_check_filetype_and_ext', 'check_json_filetype', 10, 4);
-
-// Allow JSON files to bypass the upload restriction in WordPress
-function allow_unfiltered_json_upload($file) {
-    // Check for JSON file type
-    if ($file['type'] === 'application/json') {
-        // No error for JSON files
-        $file['error'] = false;
-    }
-    return $file;
-}
-add_filter('wp_handle_upload_prefilter', 'allow_unfiltered_json_upload');
-
-// Display JSON files properly in the Media Library (optional)
-function enable_json_preview_in_media_library($response, $attachment, $meta) {
-    // Ensure the file is a JSON file
-    if ($response['mime'] === 'application/json') {
-        // Provide a basic preview message for JSON files
-        $response['uploaded_filename'] = basename(get_attached_file($attachment->ID));
-        $response['url'] = wp_get_attachment_url($attachment->ID);
-    }
-    return $response;
-}
-add_filter('wp_prepare_attachment_for_js', 'enable_json_preview_in_media_library', 10, 3);
+require_once get_stylesheet_directory() . '/includes/utils.php';
