0efaa8255852a01dbedba86634de83abd5eb1bb7
sinanisler <sinan@sinanisler.com>
2025-03-06 23:01:20 +0300

    BUG FIX: Trying to fix this  #47 there was already bricks check for name but there might be an issue with native wp core func lets test what will happen with this on next release.

--- includes/auto-update-bricks.php
+++ includes/auto-update-bricks.php
@@ -1,29 +1,34 @@
 <?php
 
 function snn_auto_update_bricks_theme($update, $item) {
-  $options = get_option('snn_settings');
-  if (isset($options['auto_update_bricks']) && $item->theme == 'bricks') {
-    return true;
-  }
-  return $update;
+    $options = get_option('snn_settings');
+    
+    // Check if the auto-update option is enabled and that the theme slug is exactly 'bricks'.
+    // Note: Ensure that the property you're checking matches your theme's update object. 
+    // Often the theme directory name is stored in $item->theme.
+    if ( isset($options['auto_update_bricks']) && $options['auto_update_bricks'] == 1 && isset($item->theme) && 'bricks' === $item->theme ) {
+        return true;
+    }
+    
+    return $update;
 }
 add_filter('auto_update_theme', 'snn_auto_update_bricks_theme', 10, 2);
 
 function snn_auto_update_bricks_setting_field() {
-  add_settings_field(
-    'snn_auto_update_bricks',
-    'Auto Update Bricks Theme (Bricks Builder Only)',
-    'snn_auto_update_bricks_callback',
-    'snn-settings',
-    'snn_general_section'
-  );
+    add_settings_field(
+        'snn_auto_update_bricks',
+        'Auto Update Bricks Theme (Bricks Builder Only)', 
+        'snn_auto_update_bricks_callback',
+        'snn-settings',
+        'snn_general_section'
+    );
 }
 add_action('admin_init', 'snn_auto_update_bricks_setting_field');
 
 function snn_auto_update_bricks_callback() {
-  $options = get_option('snn_settings');
-  ?>
-  <input type="checkbox" name="snn_settings[auto_update_bricks]" value="1" <?php checked(isset($options['auto_update_bricks']), 1); ?>>
-  <p>Enabling this setting will automatically update the Bricks theme whenever a new version is available.</p>
-  <?php
+    $options = get_option('snn_settings');
+    ?>
+    <input type="checkbox" name="snn_settings[auto_update_bricks]" value="1" <?php checked(isset($options['auto_update_bricks']), 1); ?>>
+    <p>Enabling this setting will automatically update the Bricks theme whenever a new version is available.</p>
+    <?php
 }
