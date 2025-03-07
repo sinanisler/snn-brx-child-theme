eacc1406d6de9c17c4a6f843cfad6b77c82f4bba
sinanisler <sinan@sinanisler.com>
2025-03-07 04:10:24 +0300

    IMPROVEMENT: custom svg path upload support for the animated heading. Needs some basic figma or any vector design program knowledge.

--- includes/elements/animated-heading.php
+++ includes/elements/animated-heading.php
@@ -185,9 +185,9 @@ class Animated_Headline_Element extends Element {
 
         // DOM Element Tag Control for selecting h1, h2, h3, etc.
         $this->controls['dom_element_tag'] = [
-            'tab'     => 'content',
-            'type'    => 'select',
-            'options' => [
+            'tab'         => 'content',
+            'type'        => 'select',
+            'options'     => [
                 'h1'   => esc_html__('H1', 'bricks'),
                 'h2'   => esc_html__('H2', 'bricks'),
                 'h3'   => esc_html__('H3', 'bricks'),
@@ -196,7 +196,15 @@ class Animated_Headline_Element extends Element {
                 'span' => esc_html__('Span', 'bricks'),
                 'div'  => esc_html__('Div', 'bricks'),
             ],
-            'placeholder' => 'div'
+            'placeholder' => 'div',
+        ];
+
+        // New control for custom SVG upload.
+        $this->controls['custom_svg'] = [
+            'tab'         => 'content',
+            'type'        => 'svg',
+            'label'       => esc_html__('Custom SVG', 'bricks'),
+            'description' => esc_html__('Upload your own SVG path. <br>Create 510px x 170px transparent rectangle and draw your path inside of that. Group it export it.', 'bricks'),
         ];
     }
 
@@ -291,9 +299,13 @@ class Animated_Headline_Element extends Element {
             <span class="snn-headline-wrapper">
                 <span class="snn-headline-text"><?php echo $dynamic_text; ?></span>
                 <svg class="animated-headline-svg" xmlns="http://www.w3.org/2000/svg" viewBox="-10 -14 520 178" preserveAspectRatio="none">
-                    <?php foreach ($paths as $d) : ?>
-                        <path d="<?php echo esc_attr($d); ?>" stroke="<?php echo $svg_color; ?>" fill="none" <?php if ($rounded) { echo 'stroke-linecap="round" stroke-linejoin="round"'; } ?>></path>
-                    <?php endforeach; ?>
+                    <?php if (isset($this->settings['custom_svg']['url']) && !empty($this->settings['custom_svg']['url'])): ?>
+                        <?php echo file_get_contents( esc_url($this->settings['custom_svg']['url']) ); ?>
+                    <?php else: ?>
+                        <?php foreach ($paths as $d) : ?>
+                            <path d="<?php echo esc_attr($d); ?>" stroke="<?php echo $svg_color; ?>" fill="none" <?php if ($rounded) { echo 'stroke-linecap="round" stroke-linejoin="round"'; } ?>></path>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
                 </svg>
             </span>
             <span class="snn-headline-plain-text"><?php echo $after_text; ?></span>
@@ -361,19 +373,25 @@ class Animated_Headline_Element extends Element {
                             height: element.settings.svg_height || '1.7em',
                             'stroke-width': element.settings.stroke_width || '8px'
                         }, element.settings.text_on_top ? { zIndex: 0 } : {}]">
-                        <!-- Inline mapping of svg_option to path data -->
-                        <template v-for="(d, index) in ({
-                            'option1': ['M9.3,127.3c49.3-3,150.7-7.6,199.7-7.4 c121.9,0.4,189.9,0.4,282.3,7.2 C380.1,129.6,181.2,130.6,70,139 c82.6-2.9,254.2-1,335.9,1.3 c-56,1.4-137.2-0.3-197.1,9'],
-                            'option2': ['M325,18C228.7-8.3,118.5,8.3,78,21C22.4,38.4,4.6,54.6,5.6,77.6c1.4,32.4,52.2,54,142.6,63.7 c66.2,7.1,212.2,7.5,273.5-8.3c64.4-16.6,104.3-57.6,33.8-98.2C386.7-4.9,179.4-1.4,126.3,20.7'],
-                            'option3': ['M3,146.1c17.1-8.8,33.5-17.8,51.4-17.8c15.6,0,17.1,18.1,30.2,18.1c22.9,0,36-18.6,53.9-18.6 c17.1,0,21.3,18.5,37.5,18.5c21.3,0,31.8-18.6,49-18.6c22.1,0,18.8,18.8,36.8,18.8c18.8,0,37.5-18.6,49-18.6c20.4,0,17.1,19,36.8,19 c22.9,0,36.8-20.6,54.7-18.6c17.7,1.4,7.1,19.5,33.5,18.8c17.1,0,47.2-6.5,61.1-15.6'],
-                            'option4': ['M7.7,145.6C109,125,299.9,116.2,401,121.3c42.1,2.2,87.6,11.8,87.3,25.7'],
-                            'option5': ['M8.4,143.1c14.2-8,97.6-8.8,200.6-9.2c122.3-0.4,287.5,7.2,287.5,7.2', 'M8,19.4c72.3-5.3,162-7.8,216-7.8c54,0,136.2,0,267,7.8'],
-                            'option6': ['M3,75h493.5'],
-                            'option7': ['M497.4,23.9C301.6,40,155.9,80.6,4,144.4', 'M14.1,27.6c204.5,20.3,393.8,74,467.3,111.7']
-                        }[ element.settings.svg_option || 'option4' ] )" :key="index">
-                            <path :d="d" :stroke="element.settings.svg_color || '#000000'" fill="none" 
-                                  :stroke-linecap="element.settings.rounded ? 'round' : ''" 
-                                  :stroke-linejoin="element.settings.rounded ? 'round' : ''"></path>
+                        <template v-if="element.settings.custom_svg && element.settings.custom_svg.url">
+                            <g>
+                                <text x="0" y="50" fill="#000">Custom SVG preview not available</text>
+                            </g>
+                        </template>
+                        <template v-else>
+                            <template v-for="(d, index) in ({
+                                'option1': ['M9.3,127.3c49.3-3,150.7-7.6,199.7-7.4 c121.9,0.4,189.9,0.4,282.3,7.2 C380.1,129.6,181.2,130.6,70,139 c82.6-2.9,254.2-1,335.9,1.3 c-56,1.4-137.2-0.3-197.1,9'],
+                                'option2': ['M325,18C228.7-8.3,118.5,8.3,78,21C22.4,38.4,4.6,54.6,5.6,77.6c1.4,32.4,52.2,54,142.6,63.7 c66.2,7.1,212.2,7.5,273.5-8.3c64.4-16.6,104.3-57.6,33.8-98.2C386.7-4.9,179.4-1.4,126.3,20.7'],
+                                'option3': ['M3,146.1c17.1-8.8,33.5-17.8,51.4-17.8c15.6,0,17.1,18.1,30.2,18.1c22.9,0,36-18.6,53.9-18.6 c17.1,0,21.3,18.5,37.5,18.5c21.3,0,31.8-18.6,49-18.6c22.1,0,18.8,18.8,36.8,18.8c18.8,0,37.5-18.6,49-18.6c20.4,0,17.1,19,36.8,19 c22.9,0,36.8-20.6,54.7-18.6c17.7,1.4,7.1,19.5,33.5,18.8c17.1,0,47.2-6.5,61.1-15.6'],
+                                'option4': ['M7.7,145.6C109,125,299.9,116.2,401,121.3c42.1,2.2,87.6,11.8,87.3,25.7'],
+                                'option5': ['M8.4,143.1c14.2-8,97.6-8.8,200.6-9.2c122.3-0.4,287.5,7.2,287.5,7.2', 'M8,19.4c72.3-5.3,162-7.8,216-7.8c54,0,136.2,0,267,7.8'],
+                                'option6': ['M3,75h493.5'],
+                                'option7': ['M497.4,23.9C301.6,40,155.9,80.6,4,144.4', 'M14.1,27.6c204.5,20.3,393.8,74,467.3,111.7']
+                            }[ element.settings.svg_option || 'option4' ] )" :key="index">
+                                <path :d="d" :stroke="element.settings.svg_color || '#000000'" fill="none" 
+                                      :stroke-linecap="element.settings.rounded ? 'round' : ''" 
+                                      :stroke-linejoin="element.settings.rounded ? 'round' : ''"></path>
+                            </template>
                         </template>
                     </svg>
                 </span>
