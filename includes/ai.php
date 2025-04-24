4434f379f878fd2f99eaa6e0604baa04a161d4a0
sinanisler <sinan@sinanisler.com>
2025-04-24 10:10:54 +0300

    CHANGE: changing the default openai model o-4o-mini to 4.1-mini. It is much better smarter and has more memory. Best for website use cases and cheaper too.

--- includes/ai.php
+++ includes/ai.php
@@ -33,7 +33,7 @@ function snn_render_ai_settings() {
     $ai_enabled         = get_option('snn_ai_enabled', 'no');
     $ai_provider        = get_option('snn_ai_provider', 'openai');
     $openai_api_key     = get_option('snn_openai_api_key', '');
-    $openai_model       = get_option('snn_openai_model', 'gpt-4o-mini');
+    $openai_model       = get_option('snn_openai_model', 'gpt-4.1-mini');
     $openrouter_api_key = get_option('snn_openrouter_api_key', '');
     $openrouter_model   = get_option('snn_openrouter_model', '');
     $system_prompt      = get_option(
