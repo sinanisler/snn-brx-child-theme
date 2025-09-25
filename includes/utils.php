f66d2fd109a5dfdc9a21093cc5bf317c760c31cc
sinanisler <sinan@sinanisler.com>
2025-09-25 11:36:40 +0300

    CHANGE: Update color palette grid template column size in footer CSS for Bricks Editor

--- includes/utils.php
+++ includes/utils.php
@@ -65,7 +65,7 @@ function snn_custom_css_utils_wp_footer() {
 			float:left;
 		}
 
-        [data-control=color] .bricks-control-popup .color-palette{ grid-template-columns: repeat(auto-fit,minmax(32px,32px)) ; }
+        [data-control=color] .bricks-control-popup .color-palette{ grid-template-columns: repeat(auto-fit,minmax(36px,36px)) ; }
 
     </style>
     <?php
