32776501585511ae59f1f6e5ec6da0528ff95b29
sinanisler <sinan@sinanisler.com>
2025-12-24 15:37:17 +0300

    .

--- includes/elements/element-event-action-selector.php
+++ includes/elements/element-event-action-selector.php
@@ -304,10 +304,7 @@ class Snn_Event_Action_Selector extends Element {
                                 el.focus({ preventScroll: false });
                                 break;
                             case 'Blur Element':
-                                 if (e && e.relatedTarget && el.contains(e.relatedTarget)) {
-                                        return; // Don't hide if focusing within
-                                    }
-                                el.blur();
+                                 setTimeout(() => el.blur(), 50);
                                 break;
                             case 'Toggle Fullscreen':
                                 if (!document.fullscreenElement) { el.requestFullscreen().catch(()=>{}); }
