8d2d622d155a6408062aee3b8e479c3179c99c14
sinanisler <sinan@sinanisler.com>
2025-12-25 14:50:46 +0300

    IMPROVEMENTS: Update sticky menu header visibility in admin dashboard for better display control

--- includes/wp-admin-dashboard-widgets.php
+++ includes/wp-admin-dashboard-widgets.php
@@ -9,7 +9,7 @@ function my_custom_fonts() {
 <style>
 .postbox-header{border-bottom:none !important; }
 .postbox{border: none !important; box-shadow: none !important; }
-.sticky-menu h1{ display:none }
+.sticky-menu h1:not(.inside h1){ display:none }
 </style>
 
 <?php
