3de0d96b33731d903b6ef5af2bbbe5a0af206063
sinanisler <sinan@sinanisler.com>
2025-12-26 00:26:43 +0300

    .

--- includes/wp-admin-dashboard-widgets.php
+++ includes/wp-admin-dashboard-widgets.php
@@ -10,7 +10,6 @@ function my_custom_fonts() {
 .postbox-header{border-bottom:none !important; }
 .postbox{border: none !important; box-shadow: none !important; }
 .sticky-menu h1:has(.index-php){ display:none }
-.sticky-menu h1:not(.inside h1){ display:block }
 </style>
 
 <?php
