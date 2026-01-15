0f482d35429ca47d296d4adf9d0ff1d5e5cdbd41
sinanisler <sinan@sinanisler.com>
2026-01-15 20:51:01 +0300

    .

--- includes/ai/ai-agent-and-chat.php
+++ includes/ai/ai-agent-and-chat.php
@@ -780,7 +780,7 @@ class SNN_Chat_Overlay {
 
         $wp_admin_bar->add_node( array(
             'id'     => 'snn-ai-chat',
-            'title'  => '<span style="font-size: 25px; background: linear-gradient(45deg, #2271b1, #e4dadd); -webkit-background-clip: text; -webkit-text-fill-color: transparent; position: relative;  line-height: 1.2;">✦</span>',
+            'title'  => '<span style="font-size: 25px; background: linear-gradient(45deg, #2271b1, #ffffff); -webkit-background-clip: text; -webkit-text-fill-color: transparent; position: relative;  line-height: 1.2;">✦</span>',
             'href'   => '#',
             'parent' => 'top-secondary',
             'meta'   => array(
