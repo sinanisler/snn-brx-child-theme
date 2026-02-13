83d88f736fabf3f9e58ef1e7fd38bc07201bfbf2
sinanisler <sinan@sinanisler.com>
2026-02-13 13:37:21 +0300

    .

--- includes/dynamic-data-tags/parent-child-count.php
+++ includes/dynamic-data-tags/parent-child-count.php
@@ -20,44 +20,31 @@ function register_parents_child_posts_count_tag( $tags ) {
     return $tags;
 }
 
-add_filter( 'bricks/dynamic_data/render_tag', 'render_parents_child_posts_count_tag', 20, 3 );
+add_filter( 'bricks/dynamic_data/render_tag', 'render_parents_child_posts_count_tag', 10, 3 );
 function render_parents_child_posts_count_tag( $tag, $post, $context = 'text' ) {
-    if ( $tag !== '{parents_child_posts_count}' ) {
+    if ( strpos( $tag, '{parents_child_posts_count}' ) === false ) {
         return $tag;
     }
 
-    $current_post_id = get_the_ID();
-
-    if ( ! $current_post_id ) {
-        return 0;
-    }
-
-    $current_post = get_post( $current_post_id );
-
-    if ( ! $current_post || ! $current_post->post_parent ) {
+    // If the post has no parent, return 0
+    if ( ! $post->post_parent ) {
         return 0;
     }
 
     // Get all children of the parent using get_children()
-    $args = [
-        'post_parent'    => $current_post->post_parent,
-        'post_type'      => $current_post->post_type,
+    $children = get_children( array(
+        'post_parent'    => $post->post_parent,
+        'post_type'      => $post->post_type,
         'post_status'    => 'publish',
         'numberposts'    => -1,
-    ];
-
-    $children = get_children( $args );
+    ) );
 
     return count( $children );
 }
 
-add_filter( 'bricks/dynamic_data/render_content', 'render_parents_child_posts_count_tag_in_content', 20, 3 );
-add_filter( 'bricks/frontend/render_data', 'render_parents_child_posts_count_tag_in_content', 20, 2 );
+add_filter( 'bricks/dynamic_data/render_content', 'render_parents_child_posts_count_tag_in_content', 10, 3 );
+add_filter( 'bricks/frontend/render_data', 'render_parents_child_posts_count_tag_in_content', 10, 2 );
 function render_parents_child_posts_count_tag_in_content( $content, $post, $context = 'text' ) {
-    if ( ! is_string( $content ) ) {
-        return $content;
-    }
-
     if ( strpos( $content, '{parents_child_posts_count}' ) !== false ) {
         $count   = render_parents_child_posts_count_tag( '{parents_child_posts_count}', $post, $context );
         $content = str_replace( '{parents_child_posts_count}', $count, $content );
